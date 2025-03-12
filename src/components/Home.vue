b939d250b67a2210cb059c62912faf1be8c8cf1c
RobertTruter-Adams <robert.truter@adams.africa>
2025-03-12 21:28:33 +0200

    #Home Page
    Add CampsBay image and update Home component layout
    
    - Introduced a new CampsBay.jpg image to the footer for enhanced visual appeal.
    - Updated global.css to include box-sizing for better layout control.
    - Enhanced Home.vue by adding a welcome section with personal introduction and social media icons, improving user engagement and aesthetics.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,7 @@ import Tab from "./ui/Tab.vue";
 import TurkeyTab from "./tabs/TurkeyTab.vue";
 import GeorgiaTab from "./tabs/GeorgiaTab.vue";
 import SouthAfricaTab from "./tabs/SouthAfricaTab.vue";
+import "primeicons/primeicons.css";
 
 const articleHeadings = [
   {
@@ -98,4 +99,43 @@ const articleHeadings = [
       </template>
     </Tab>
   </section>
+
+  <section class="flex items-center justify-center h-96 mb-20">
+    <div class="relative h-72 w-full lg:w-2/3 rounded-xl overflow-visible bg-[#99bfd7]">
+      <div
+        class="flex flex-row justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full"
+      >
+        <div
+          class="flex flex-col gap-6 items-center text-center min-h-72 min-md:h-80 lg:h-96 w-72 lg:w-80 z-20 p-5 bg-[#ebf0f2]"
+        >
+          <h2 class="text-2xl font-bold leading-10">
+            Welcome, we're Robert and
+            <span class="p-2 rounded-full text-white bg-color-primary">Kija!</span>
+          </h2>
+
+          <p>
+            We're both in our early 20's and working in tech fulltime, we try to travel as much as
+            possible while balancing our careers.
+          </p>
+
+          <p>We love slow travel and focus on inspiring the ordinary to make travel a priority</p>
+          <div class="space-x-2">
+            <span class="pi pi-facebook cursor-pointer"></span>
+            <span class="pi pi-instagram cursor-pointer"></span>
+          </div>
+        </div>
+
+        <img
+          src="/footer/CampsBay.jpg"
+          class="footerImg hidden sm:inline scale-[0.7] sm:scale-[1.1] lg:scale-[1.3] w-96 ml-[-30px] z-10"
+        />
+      </div>
+    </div>
+  </section>
 </template>
+
+<style lang="css">
+.footerImg {
+  -webkit-mask: url(https://i.ibb.co/mBrmH0k/Mf15d.png) center/contain no-repeat;
+}
+</style>
