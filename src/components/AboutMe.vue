fe3edbca3d0b96dd9150e27d316f69994df8a135
RobertTruter-Adams <robert.truter@adams.africa>
2025-03-17 21:00:24 +0200

    #About Me Page
    Enhance AboutMe component with personal introduction and improved layout
    
    - Updated AboutMe.vue to feature a new header section with a personalized introduction for Kija & Robert.
    - Added descriptive paragraphs detailing their background, travel philosophy, and blog purpose, enhancing user engagement.
    - Improved overall layout and styling for a more visually appealing presentation.

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,5 +3,30 @@ import NavBar from "./NavBar.vue";
 </script>
 
 <template>
-  <h1>About me</h1>
+  <header class="flex flex-col self-center items-center gap-10 w-full mt-10 p-5 text-center">
+    <div class="w-80 relative">
+      <h1 class="text-[35px] p-1 z-20 font-bold">We're Kija & Robert</h1>
+      <div class="absolute h-6 w-full bottom-0 -z-10 bg-color-primary"></div>
+    </div>
+
+    <section class="w-80 sm:w-[600px] mt-5 space-y-5 font-bold tracking-wider break-all">
+      <p>
+        Hi! We're Kija & Robbie from Cape Town, South Africa. By day, we work in marketing and
+        software development, and by night, we're dreaming of our next destination.
+      </p>
+
+      <p>
+        In 2023, we set out on our first big adventure to Türkiye, and it sparked a passion that
+        quickly became a lifestyle. Since then, we've been navigating the balance of working a 9-5
+        and traveling the world, capturing moments that inspire others to look beyond their
+        routines.
+      </p>
+
+      <p>
+        We're big believers in slow travel—choosing depth over speed and discovering the beauty in
+        everyday moments. Through our blog, we aim to guide and inspire you to turn your travel
+        dreams into reality.
+      </p>
+    </section>
+  </header>
 </template>
