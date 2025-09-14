4ec0f5b0279901ac6c9b6da2f96f16b42faa0ee2
Robbie-Truter <timothylang6@gmail.com>
2025-09-14 17:04:38 +0200

    #Configurations
    Typescript config clean up
    Added vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]", // Hashing for cache busting
+      },
+    },
+  },
   resolve: {
     alias: {
       "@/pages": path.resolve(__dirname, "./src/pages"),
